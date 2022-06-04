811af0f46fdd4637e53230bcc5e8d84df6ba713e
chitranshoo <chitranshoo10081998@gmail.com>
2022-06-04 17:15:52 +0530

    display recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simple test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.allrecipes.com%2Frecipe%2F212721%2Findian-chicken-curry-murgh-kari%2F&psig=AOvVaw3ckmcspcwWpKVkpT5x7BzW&ust=1654425861512000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLDeqOLOk_gCFQAAAAAdAAAAABAF'),
+    new Recipe('A Test Recipe', 'This is a simple test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.allrecipes.com%2Frecipe%2F212721%2Findian-chicken-curry-murgh-kari%2F&psig=AOvVaw3ckmcspcwWpKVkpT5x7BzW&ust=1654425861512000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLDeqOLOk_gCFQAAAAAdAAAAABAF')
+  ];
 
   constructor() { }
 
